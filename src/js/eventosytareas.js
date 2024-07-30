e02c4e7e6099938f55082e2f9432a135b7f99bc0
wendybarrantes <wenbarrantes06@gmail.com>
2024-07-30 13:56:46 -0600

    funciones agregar y mostrar tareas

--- src/js/eventosytareas.js
+++ src/js/eventosytareas.js
@@ -1,20 +1,45 @@
 let btn1 = document.getElementById("btn-agregar")
 let bnt2 = document.getElementById("bnt-editar")
 
-let tareas = JSON.parse(localStorage.getItem("tareas")) || []
+let lista = JSON.parse(localStorage.getItem("tareas")) || [] //variable global//
 
-function AgredarTareas(){
+
+function agregarTareas(){
     let nombreTarea = document.getElementById("inp-tarea").value
-    let prioridadTarea= document.getElementById(inp-prioridad).value
+    let prioridadTarea = document.getElementById("inp-prioridad").value
 
     //objeto//
-
-
     let tarea = {
-        nombre: nombreTarea;
-        prioridad: prioridadTarea;
+        nombre: nombreTarea,
+        prioridad: prioridadTarea
     }
-
-        listaTareas.push(tareas)
-
+    lista.push(tarea)
+    localStorage.setItem("tareas",JSON.stringify(lista))
 }
+function mostrarTareas() {
+    let contenedorDeTareas = document.getElementById("contenedor")
+    contenedorDeTareas.innerHTML= "" 
+    lista.forEach((tarea,indice)=>{
+    let li = document.createElement("li")    
+                       //interpolacion//                          
+    li.textContent = `La tarea es: ${tarea.nombre}         
+     la prioridad es: ${tarea.prioridad}`
+
+    let botonEditar = document.createElement("button")
+        botonEditar.textContent = "Editar"
+        botonEditar.addEventListener("click", ()=>editarTarea(indice))
+
+    let botonEliminar = document.createElement("button")
+        botonEliminar.textContent = "Eliminar"
+        botonEliminar.addEventListener("click",()=>eliminarTarea(indice))
+
+    li.appendChild(botonEditar)
+    li.appendChild(botonEliminar)
+    contenedorDeTareas.appendChild(li)
+    })
+}
+mostrarTareas()
+btn1.addEventListener("click",agregarTareas)
+
+
+
