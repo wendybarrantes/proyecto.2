cacf700ca16e5505ac062c9a8a7319fa03c83748
wendybarrantes <wenbarrantes06@gmail.com>
2024-07-30 17:30:51 -0600

    inicio de funcion confirmar

--- src/js/eventosytareas.js
+++ src/js/eventosytareas.js
@@ -4,7 +4,7 @@ let bnt2 = document.getElementById("bnt-editar")
 let lista = JSON.parse(localStorage.getItem("tareas")) || [] //variable global//
 
 
-function agregarTareas(){
+ function agregarTareas(){
     let nombreTarea = document.getElementById("inp-tarea").value
     let prioridadTarea = document.getElementById("inp-prioridad").value
 
@@ -16,6 +16,7 @@ function agregarTareas(){
     lista.push(tarea)
     localStorage.setItem("tareas",JSON.stringify(lista))
 }
+
 function mostrarTareas() {
     let contenedorDeTareas = document.getElementById("contenedor")
     contenedorDeTareas.innerHTML= "" 
@@ -38,6 +39,35 @@ function mostrarTareas() {
     contenedorDeTareas.appendChild(li)
     })
 }
+
+function eliminarTarea(indice){
+    lista.splice(indice,1)
+    localStorage.setItem("tareas",JSON.stringify(lista))  //PREGUNTAR//
+    mostrarTareas()
+}
+
+function editarTarea(indice){
+let editarInput = document.getElementByI("inp-edita-tarea")
+let editarPrioridad = documen("inp-edita-prioridad")
+
+editarInput.style.stile="block"  //PREGUNTAR//
+editarPrioridad.style.stile="block"
+botonEditar.style.stile="block"
+
+editarInput.value = lista(indice).nombre
+editarPrioridad.value = lista(indice).prioridad
+
+botonEditar.addEventListener("click",()=>ConfirmarEdicion(indice))
+}
+
+function ConfirmarEdicion(indice){
+
+}
+
+
+
+
+
 mostrarTareas()
 btn1.addEventListener("click",agregarTareas)
 
