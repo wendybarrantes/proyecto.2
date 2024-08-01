4c92c4d848db3e6ec6766e5cd90e923be474ed14
wendybarrantes <wenbarrantes06@gmail.com>
2024-07-31 23:40:38 -0600

    registro

---
+++ src/js/registro.js
@@ -0,0 +1,31 @@
+let btnCrearCuenta = document.getElementById("btnCrearCuenta")
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+let inspeccion = false 
+
+
+function crearUsuario(evento) {
+    evento.preventDefault()
+   let inpClave = document.getElementById("inp-contrasena-Cuenta").value
+   let inpCorreo = document.getElementById("inp-correo-cuenta").value
+
+   if (inpClave && inpCorreo) {
+    for (let index = 0; index < usuarios.length; index++) {
+        if (usuarios[index].correo === inpCorreo) {
+            inspeccion = true
+            break
+        }
+    }
+   }
+   if (inspeccion) {
+    alert("El usuarios ya existe")
+   }else{
+    let usuario ={
+        correo: inpCorreo,
+        contrasena: inpClave
+    }
+    usuarios.push(usuario)
+    localStorage.setItem("usuarios",JSON.stringify(usuarios))
+    alert("usuario creado")
+   }
+}
+btnCrearCuenta.addEventListener("click", crearUsuario)
